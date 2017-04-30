b35bb60c7f8a985abfdce88a7a91088b0701e974
herdiandkun <herdiandkun@gmail.com>
2017-04-30 13:52:46 +0700

    second commit

--- app/src/main/java/org/micro/amin/profile/DetailRiwayatActivity.java
+++ app/src/main/java/org/micro/amin/profile/DetailRiwayatActivity.java
@@ -2,8 +2,13 @@ package org.micro.amin.profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class DetailRiwayatActivity extends AppCompatActivity {
 
@@ -15,12 +20,38 @@ public class DetailRiwayatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_riwayat);
 
         lv_riwayat_pendidikan = (ListView) findViewById(R.id.lv_riwayat_pendidikan);
-        String riwayat[] = {"SDN xxxx",
+        final String riwayat[] = {"SDN xxxx",
                             "SMPN xxxx",
                             "SMAN xxxx",
                             "IPB"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, riwayat);
         lv_riwayat_pendidikan.setAdapter(adapter);
+
+        lv_riwayat_pendidikan.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(DetailRiwayatActivity.this,riwayat[position],Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+
+        if (id == R.id.menu_exit) {
+            finish();
+            System.exit(0);
+        }
+         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/org/micro/amin/profile/MainActivity.java
+++ app/src/main/java/org/micro/amin/profile/MainActivity.java
@@ -1,16 +1,43 @@
 package org.micro.amin.profile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "MainActivity";
+    Button btn_riwayat,btn_call;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_riwayat = (Button) findViewById(R.id.btn_riwayat);
+        btn_call = (Button) findViewById(R.id.btn_call);
         Log.d(TAG, "onCreate");
+
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView telepon = (TextView) findViewById(R.id.telepon);
+                String toDial = "tel:"+telepon.getText().toString();
+                Intent call =  new Intent(Intent.ACTION_DIAL, Uri.parse(toDial));
+                startActivity(call);
+            }
+        });
+
+        btn_riwayat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(MainActivity.this,DetailRiwayatActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 
     @Override
